a8e18ed9670b4298d97272cd00163d8f6dfbb942
Alex Camargo <alexcamargo13@gmail.com>
2020-06-29 14:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ function shout(string){
   return string.toUpperCase()
 }
 
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log( string.toUpperCase())
+}
+
 shout(hello)
+whisper(HELLO)
+logShout(hello)
