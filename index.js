c84935dafe954d168550b553b044dbb6a4519c89
Alex Camargo <alexcamargo13@gmail.com>
2020-06-29 14:40:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function logShout(string){
   console.log( string.toLowerCase())
 }
 function sayHiToGrandma(string){
+  if(string === "I love you, Grandma."){
+    return ""
+  }
   if (string === string.toLowerCase()){
    return "I can't hear you!" 
   }
