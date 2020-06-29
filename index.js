3c838e765d47a70ab6d13ac289a509f55324636b
Alex Camargo <alexcamargo13@gmail.com>
2020-06-29 14:38:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function whisper(string){
 function logShout(string){
   console.log( string.toUpperCase())
 }
+function logShout(string){
+  console.log( string.toLowerCase())
+}
+function sayHiToGrandma(string){
+  if string === string.toLowerCase()
+}
 
 shout(hello)
 whisper(HELLO)
